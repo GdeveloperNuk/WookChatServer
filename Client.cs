01662a5ea94a80fb9808fa8fbe5a7922771a35e7
jwlee <jwlee@endcs.net>
2024-12-13 17:17:26 +0900

    [main] 서버 초기 세팅

---
+++ Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WookChat.SERVER
+{
+	public partial class Client
+	{
+		public TcpClient client { get; set; }
+		public byte[] data { get; set; }
+		public int clientID;
+
+		public Client(TcpClient client)
+		{
+			this.client = client;
+			this.data = new byte[1024];
+
+			// 아래부분이 1:1비동기서버에서 추가된 부분입니다.
+			// 127.0.0.1:9999에서 포트번호 직전 마지막번호를 클라이언트 번호로 지정해줍니다.
+			string clientEndPoint = client.Client.LocalEndPoint.ToString();
+			char[] point = { '.', ':' };
+			string[] splitedData = clientEndPoint.Split(point);
+			this.clientID = int.Parse(splitedData[3]);
+			Console.WriteLine("{0}번 사용자 접속성공", clientID);
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,31 +1,70 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace myWebApp
+namespace WookChat.SERVER.Sources
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-        
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            string port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-            string url = String.Concat("http://0.0.0.0:", port);
+	internal class Server
+	{
+		private static IPAddress serverIp = IPAddress.Parse("192.168.10.199");
+		private static int port = 9999;
+		private static TcpListener server;
+		private static TcpClient client;
+		private static NetworkStream stream;
 
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().UseUrls(url);
-                });
-        }
-    }
+		static void Main()
+		{
+			Console.WriteLine("서버 콘솔창");
+			Console.WriteLine("===========");
+
+			//서버 오픈
+			server = new TcpListener(serverIp, port);
+			server.Start();
+
+			//서버 기능 쓰레드
+			Thread serverRun = new Thread(ServerRun);
+			serverRun.Start();
+		}
+
+		/// <summary>
+		/// 서버 기능 쓰레드
+		/// </summary>
+		private static void ServerRun()
+		{
+			while (true)
+			{
+				try
+				{
+					client = server.AcceptTcpClient();
+
+					Client clientData = new Client(client);
+
+					clientData.client.GetStream().BeginRead(clientData.data, 0, clientData.data.Length, new AsyncCallback(DataReceived), clientData);
+
+				}
+				catch (SocketException ex)
+				{
+					Console.WriteLine($"서버 오류 : {ex}");
+					break;	
+				}
+			}
+
+		}
+		private static void DataReceived(IAsyncResult ar)
+		{
+			Client callbackClient = ar.AsyncState as Client;
+
+			int bytesRead = callbackClient.client.GetStream().EndRead(ar);
+
+			string readString = Encoding.Default.GetString(callbackClient.data, 0, bytesRead);
+
+			Console.WriteLine("{0}번 사용자 : {1}", callbackClient.clientID, readString);
+
+			callbackClient.client.GetStream().BeginRead(callbackClient.data, 0, callbackClient.data.Length, new AsyncCallback(DataReceived), callbackClient);
+		}
+	}
 }
